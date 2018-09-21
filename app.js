d4ed10f5249b2a6b13e6f346e8e7e6f577bfaf7b
afmire877 <33981371+afmire877@users.noreply.github.com>
2018-09-22 00:28:58 +0100

    Added a function for shuffling

--- app.js
+++ app.js
@@ -1,45 +1,60 @@
-var images = document.querySelectorAll("img.image");
+var cards = document.querySelectorAll("td.card p");
+var box = document.querySelectorAll("td.card");
+var arr = [1,1,2,2,3,3,4,4,5,5,6,6];
+
+// let isFlippedCard = flase;
+// let firstCard, secondCard;
+
+function init(){
+
+}
 
 
-var image = [
-	"png/black-and-white-whistle-variant.png",
-	"png/checkered-flags.png",
-	"png/bees-panel-texture.png",
-	"png/couple-of-football-cones.png",
-	"png/flaming-football.png",
-	"png/football-award-with-white-details.png",
-	"png/football-badge.png",
-	"png/football-ball.png", 
-	"png/football-championship-award-with-plus-sign.png",
-	"png/football-coach-screaming.png",
-	"png/football-court-illumination-lamps.png",
-	"png/football-fan-raising-flag.png",
-	"png/football-fans-group.png",
-
-];
 
 
 function setupCards(){
-	for (var i=0;i <= image.length;i++){
-		var random = randomNumber();
-		images[i].src = image[random];
-	
+	for(i=0; i<= arr.length;i++){
+		console.log(cards[i])	
+		cards[i].insertAdjacentHTML("beforeend",arr[i].toString());
+	}
+};
+
+function clicked(){
+	for(var i=0;i <= arr.length ;i++){
+	 	box[i].addEventListener("click", function(){
+	 		this.classList.toggle("clicked");
+	 		// var p = this.firstChild;
+	 		// p.classList.toggle("clicked");
+	 	});
 	};
+}
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 };
 
- // for(var i=0;i<= images.length;i++){
- // 	images[i].addEventListener("click", function(){
- // 		console.log(this.src);
- // 	});
- // }
-
-function randomimg(){
-
-};
 function randomNumber(){
-	var random = Math.floor(Math.random() * 10);
+	var random = Math.floor(Math.random() * 7);
 	return random
 };
 
-console.log(image)
+arr = shuffle(arr);
+console.log(arr);
 setupCards();
+clicked();
